80c9a29dd4c50719dae25ac713bb59778d847644
Konrad Pękala <kgpekala@gmail.com>
2021-04-15 21:08:32 +0200

    lab3: addedd compressing

--- lab3/main.py
+++ lab3/main.py
@@ -6,10 +6,24 @@ class Symbol:
         self.value = value
         self.freq = freq
 
+class Node:
+    def __init__(self, symbol: Symbol):
+        self.symbol = symbol
+        self.leftChild = None
+        self.rightChild = None
+
+    def __lt__(self, other):
+        return self.symbol.freq < other.symbol.freq
+
 
 class HuffmanTree:
     def __init__(self, text):
         self.root = self.huffman_coding(self.create_symbols(text))
+        self.codes = {}
+        self.create_codes()
+        #testing below
+        for key, value in self.codes.items():
+            print(key,value)
 
     def huffman_coding(self, symbols):
         queue = []
@@ -34,24 +48,40 @@ class HuffmanTree:
         symbols = [Symbol(a, weight) for a, weight in symbols_map.items()]
         return symbols
 
-class Node:
-    def __init__(self, symbol: Symbol):
-        self.symbol = symbol
-        self.leftChild = None
-        self.rightChild = None
+    def codes_recur(self, itr: Node, code):
+        value = itr.symbol.value
+        if value is not None:
+            self.codes[value] = code
+        else:
+            self.codes_recur(itr.leftChild, code + "0")
+            self.codes_recur(itr.rightChild, code + "1")
 
-    def __lt__(self, other):
-        return self.symbol.freq < other.symbol.freq
+    def create_codes(self):
+        itr = self.root
+        self.codes_recur(itr.leftChild, "0")
+        self.codes_recur(itr.rightChild, "1")
+
+    def get_code(self, symbol_value):
+        return self.codes[symbol_value]
+
+
 
 class Compressor:
     def __init__(self, text):
         self.tree = HuffmanTree(text)
-    def compress(self):
+        self.text = text
+    def compress(self,):
         b = bitarray()
-
+        for letter in self.text:
+            code = self.tree.codes[letter]
+            b += bitarray(code)
+        return b
 
 if __name__ == '__main__':
-    tree = HuffmanTree("")
+    cmpr = Compressor("asbcabscbab")
+    print(cmpr.compress())
+    #0100010000011000
+    #
 
 
 
